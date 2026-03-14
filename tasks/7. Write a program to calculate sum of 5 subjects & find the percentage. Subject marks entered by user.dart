612318a8cb328ed61535a2810272cb812a3cc6c9
riddhita5 <riddhitagosai5@gmail.com>
2026-03-15 00:31:01 +0530

    Create 7. Write a program to calculate sum of 5 subjects & find the percentage. Subject marks entered by user.dart

---
+++ tasks/7. Write a program to calculate sum of 5 subjects & find the percentage. Subject marks entered by user.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main()
+{
+  print("Enter Your English marks");
+  int a=int.parse(stdin.readLineSync().toString());
+  print("Enter Your Hindi marks");
+  int b=int.parse(stdin.readLineSync().toString());
+  print("Enter Your Maths marks");
+  int c=int.parse(stdin.readLineSync().toString());
+  print("Enter Your Science marks");
+  int d=int.parse(stdin.readLineSync().toString());
+  print("Enter Your Social marks");
+  int e=int.parse(stdin.readLineSync().toString());
+
+  var total= a+b+c+d+e;
+  print(total);
+  int double =500;
+  var ans = (a+b+c+d+e)*100/500;
+  print(ans);
+
+}
