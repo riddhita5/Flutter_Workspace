e5f49b13bdee37497c8826abc5d6e22e8bbf3b43
riddhita5 <riddhitagosai5@gmail.com>
2026-03-14 19:54:20 +0530

    Update 2. Write a program to make addition, Subtraction, Multiplication and Division of Two Numbers.[User input].dart

--- tasks/2. Write a program to make addition, Subtraction, Multiplication and Division of Two Numbers.[User input].dart
+++ tasks/2. Write a program to make addition, Subtraction, Multiplication and Division of Two Numbers.[User input].dart
@@ -1,1 +1,24 @@
+import 'dart:io';
 
+void main()
+{
+
+  print("Enter first number:");
+  int a = int.parse(stdin.readLineSync()!);
+
+  print("Enter second number:");
+  int b = int.parse(stdin.readLineSync()!);
+
+  int add = a+b;
+  print(add);
+
+  int sub = a-b;
+  print(sub);
+
+  int mul = a*b;
+  print(mul);
+
+  var div = a/b;
+  print(div);
+
+}
