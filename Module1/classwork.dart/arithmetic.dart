6f522b15eedbc940edd3e8e2be089b0e393c321e
riddhita5 <riddhitagosai5@gmail.com>
2026-03-16 13:50:04 +0530

    Create arithmetic.dart

---
+++ Module1/classwork.dart/arithmetic.dart
@@ -0,0 +1,23 @@
+void main()
+
+{
+
+  int a = 6;
+  int b = 3;
+
+  int add = a+b;
+  print(add);
+
+  int sub = a-b;
+  print(sub);
+
+  int mul = a*b;
+  print(mul);
+
+  var div = a/b;
+  print(div);
+
+  var mod = a%b;
+  print(mod);
+
+}
