217b90e562c3fb884218a5fbc984ecb31b29c2bb
riddhita5 <riddhitagosai5@gmail.com>
2026-03-24 12:26:07 +0530

    Update pattern1.dart

--- 23-3 classwork/pattern1.dart
+++ 23-3 classwork/pattern1.dart
@@ -1,1 +1,14 @@
+void main()
+{
+  int row;
+  int col;
 
+  for(row=1;row<=5;row++)
+    {
+      for(col=1;col<=5;col++)
+        {
+          print("[$row][$col]");
+        }
+      print("");
+    }
+}
