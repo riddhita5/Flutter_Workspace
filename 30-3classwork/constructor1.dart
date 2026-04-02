084770e80f633446513ad5f8a9f284bd3a02e123
riddhita5 <riddhitagosai5@gmail.com>
2026-04-02 13:36:21 +0530

    Create constructor1.dart

---
+++ 30-3classwork/constructor1.dart
@@ -0,0 +1,14 @@
+//default constructor example
+class Student
+{
+  Student()
+  {
+    print("Hello");
+  }
+}
+void main()
+{
+  Student s1 = Student();
+   //s1.Student1();
+  print("Welcome");
+}
