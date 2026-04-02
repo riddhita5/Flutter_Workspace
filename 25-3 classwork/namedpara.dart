7d96f9e44b0e19a80705ea4e1b78e44d7fdd3ddc
riddhita5 <riddhitagosai5@gmail.com>
2026-04-02 13:31:20 +0530

    Create namedpara.dart

---
+++ 25-3 classwork/namedpara.dart
@@ -0,0 +1,12 @@
+details({ var name,var surname,var email,var city})
+{
+  print("My Name is $name");
+  print("My Surname is $surname");
+  print("My Email is $email");
+  print("My City is $city");
+}
+
+void main()
+{
+  print(details(surname: "Gosai",name: "Riddhita",email: "riddhita@gmail.com",city: "Jetpur"));
+}
