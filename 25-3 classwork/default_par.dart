5a013974b039c7401846229b0a1705571dfc9f8c
riddhita5 <riddhitagosai5@gmail.com>
2026-04-02 13:31:43 +0530

    Add files via upload

---
+++ 25-3 classwork/default_par.dart
@@ -0,0 +1,11 @@
+details(String name,String surname,{String city ="Jetpur"})
+{
+  print("Your Name is $name");
+  print("Your Surname is $surname");
+  print("Your City is $city");
+}
+
+void main()
+{
+  details("Riddhita", "Gosai");
+}
---
+++ 25-3 classwork/optional_para.dart
@@ -0,0 +1,12 @@
+details(String name,String surname,[var email,var city])
+{
+  print("Your Name is $name");
+  print("Your Surname is $surname");
+  print("Your Email is $email");
+  print("Your City is $city");
+}
+
+void main()
+{
+  details("Riddhita", "Gosai", "riddhita@gmail.com");
+}
