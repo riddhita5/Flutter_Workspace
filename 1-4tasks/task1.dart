bf65b623eeb73b6e7480dbabd3647e97042efdc8
riddhita5 <riddhitagosai5@gmail.com>
2026-04-04 14:19:35 +0530

    Update task1.dart

--- 1-4tasks/task1.dart
+++ 1-4tasks/task1.dart
@@ -2,21 +2,11 @@ import 'dart:io';
 
 class Shape
 {
-  void shape()
+  var a,b;
+  void shape(var a1,var b1)
   {
-    int a,b;
-    print("Enter Your value of a");
-    a = int.parse(stdin.readLineSync().toString());
-
-    print("Enter Your value of b");
-    b = int.parse(stdin.readLineSync().toString());
-
-    var ans = a*b;
-    print("Area of rectangle = $ans");
-
-    var answer = 0.5*a*b;
-    print("Area of Triangle = $answer");
-
+    a = a1;
+    b = b1;
   }
 }
 
@@ -24,6 +14,8 @@ class Rectangle extends Shape
 {
   void rect()
   {
+    var ans = a*b;
+    print("Area of Rectangle is $ans");
   }
 }
 
@@ -31,16 +23,25 @@ class Triangle extends Shape
 {
   void triangle()
   {
-
+    var ans = 0.5*a*b;
+    print("Area of Triangle is $ans");
   }
 }
 
 void main()
 {
+  var a,b;
   Rectangle rect = Rectangle();
   Triangle triangle = Triangle();
 
-  rect.shape();
+  print("Enter Your value of a");
+  a = int.parse(stdin.readLineSync().toString());
+
+  print("Enter Your value of b");
+  b = int.parse(stdin.readLineSync().toString());
+  rect.shape(a,b);
   rect.rect();
+
+  triangle.shape(a,b);
   triangle.triangle();
 }
