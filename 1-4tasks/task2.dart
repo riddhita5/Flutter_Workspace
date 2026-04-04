c073fa3b1ec1c806060d38a8066dd96ee4af18d0
riddhita5 <riddhitagosai5@gmail.com>
2026-04-04 14:22:26 +0530

    Update task2.dart

--- 1-4tasks/task2.dart
+++ 1-4tasks/task2.dart
@@ -2,63 +2,86 @@ import 'dart:io';
 
 class A
 {
-  void a()
+  var a;
+  void data(var a1)
   {
-    int a,b,c;
-    print("Enter Your value of a");
-    a = int.parse(stdin.readLineSync().toString());
-
-    print("Enter Your value of b");
-    b = int.parse(stdin.readLineSync().toString());
-
-    print("Enter Your value of c");
-    c = int.parse(stdin.readLineSync().toString());
-
-    var ans = a*b*c;
-    print("$a * $b * $c = $ans");
+    a = a1;
   }
 }
 class B extends A
 {
-  void b()
+  var b;
+  void data(var b1)
   {
+    b = b1;
   }
 }
 class C extends A
 {
-  void c()
+  var c;
+  void data(var c1)
   {
+    c = c1;
   }
 }
 class D implements B,C
 {
-  void d()
+  void display()
   {
+    var ans = a*b*c;
+    print(ans);
+
   }
 
   @override
-  void a()
+  var a;
+
+  @override
+  var b;
+
+  @override
+  var c;
+
+  @override
+  void data(a1)
   {
-    print("Answer");
+    a=a1;
   }
 
   @override
-  void b()
+  void data2(b1)
   {
-    print("Answer");
+    b=b1;
   }
 
   @override
-  void c()
+  void data3(c1)
   {
-    print("Answer");
+    c=c1;
   }
+
+
 }
 
 void main()
 {
-  B b = B();
-  C c = C();
-  b.b();
-  c.a();
+  var a,b,c;
+  B b1 = B();
+  C c1 = C();
+  D d = D();
+
+  print("Enter Your value of a");
+  a = int.parse(stdin.readLineSync().toString());
+
+  print("Enter Your value of b");
+  b = int.parse(stdin.readLineSync().toString());
+
+  print("Enter Your value of c");
+  c = int.parse(stdin.readLineSync().toString());
+
+  d.data(a);
+  d.data2(b);
+  d.data3(c);
+  d.display();
+
 }
