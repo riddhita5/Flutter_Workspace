305227a95c41922c23c711e69ab6c74fbc8820fe
riddhita5 <riddhitagosai5@gmail.com>
2026-04-09 21:40:10 +0530

    Create bitwaiseoperator.dart

---
+++ 6-4 classwork/bitwaiseoperator.dart
@@ -0,0 +1,23 @@
+void main()
+{
+  int a = 5;
+  int b = 7;
+
+  var c = a & b;
+  print(c);
+
+  var d = a | b;
+  print(d);
+
+  var e = a ^ b;
+  print(e);
+
+  var f = a = ~ b;
+  print(f);
+
+  var g = a << b;
+  print(g);
+
+  var h = a >> b;
+  print(h);
+}
