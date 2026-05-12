e64b7537b2a078ca56343e302137d1bb60b4e419
riddhita5 <riddhitagosai5@gmail.com>
2026-05-12 12:48:10 +0530

    Delete task10.dart

--- task10.dart
+++
@@ -1,9 +0,0 @@
-bool isPalindrome(String str) {
-  String rev = str.split('').reversed.join('');
-  return str == rev;
-}
-
-void main() {
-  print(isPalindrome("madam")); // true
-  print(isPalindrome("hello")); // false
-}
