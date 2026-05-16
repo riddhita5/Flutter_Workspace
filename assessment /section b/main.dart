9f448ff92f74ce05009655e23e5ed8e2daff9e05
riddhita5 <riddhitagosai5@gmail.com>
2026-05-16 13:23:15 +0530

    Create main.dart

---
+++ assessment /section b/main.dart
@@ -0,0 +1,20 @@
+import 'package:assessment/q1.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      home: DashboardScreen1());
+  }
+}
