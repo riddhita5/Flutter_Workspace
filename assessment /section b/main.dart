add7ede817aa35340b480bee5ff0a47ba44ca7ea
riddhita5 <riddhitagosai5@gmail.com>
2026-05-16 13:26:08 +0530

    Refactor main.dart for new dashboard implementation
    Updated imports and modified main function to run HealthDashboard.

--- assessment /section b/main.dart
+++ assessment /section b/main.dart
@@ -1,20 +1,11 @@
-import 'package:assessment/q1.dart';
+import 'package:assessment/que%201.dart';
+import 'package:assessment/que%202.dart';
+import 'package:assessment/que3.dart';
+import 'package:assessment/que4.dart';
+import 'package:assessment/task%205.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      themeMode: ThemeMode.system,
-      home: DashboardScreen1());
-  }
+void main()
+{
+  runApp(MaterialApp(home: HealthDashboard()));
 }
